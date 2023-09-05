51f4f26e8962be3adfd8bfdb7ac10e60e9bfef98
Taegwan Youn <tkyoun0421@naver.com>
2023-09-05 16:15:58 +0900

    문자열을 활용한 HTML 다루기

--- app.js
+++ app.js
@@ -3,19 +3,21 @@ const ajax = new XMLHttpRequest();
 const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
-ajax.open('GET', NEWS_URL, false);
-ajax.send();
 
-const newsFeed = JSON.parse(ajax.response);
+function getData(url) {
+    ajax.open('GET', url, false);
+    ajax.send();
+
+    return JSON.parse(ajax.response);
+}
+
+const newsFeed = getData(NEWS_URL);
 const ul = document.createElement('ul');
 
 window.addEventListener('hashchange', () => {
     const id = location.hash.slice(1);
 
-    ajax.open('GET', CONTENT_URL.replace('@id', id), false);
-    ajax.send();
-
-    const newsContent = JSON.parse(ajax.response);
+    const newsContent = getData(CONTENT_URL.replace('@id', id));
     const title = document.createElement('h1');
 
     title.innerHTML = newsContent.title;
@@ -23,15 +25,18 @@ window.addEventListener('hashchange', () => {
 });
 
 for (let i = 0; i < 10; i++) {
+    const div = document.createElement('div');
     const li = document.createElement('li');
     const a = document.createElement('a');
 
-    ul.appendChild(li);
-    li.appendChild(a);
-
-    a.href = `#${newsFeed[i].id}`;
-
-    a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`;
+    div.innerHTML = /* html */ `
+        <li>
+            <a href="#${newsFeed[i].id}">
+                ${newsFeed[i].title} (${newsFeed[i].comments_count})
+            </a>
+        </li>
+    `;
+    ul.appendChild(div.firstElementChild);
 }
 
 container.appendChild(ul);
--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -123,26 +123,26 @@ var ajax = new XMLHttpRequest();
 var content = document.createElement('div');
 var NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 var CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
-ajax.open('GET', NEWS_URL, false);
-ajax.send();
-var newsFeed = JSON.parse(ajax.response);
+function getData(url) {
+  ajax.open('GET', url, false);
+  ajax.send();
+  return JSON.parse(ajax.response);
+}
+var newsFeed = getData(NEWS_URL);
 var ul = document.createElement('ul');
 window.addEventListener('hashchange', function () {
   var id = location.hash.slice(1);
-  ajax.open('GET', CONTENT_URL.replace('@id', id), false);
-  ajax.send();
-  var newsContent = JSON.parse(ajax.response);
+  var newsContent = getData(CONTENT_URL.replace('@id', id));
   var title = document.createElement('h1');
   title.innerHTML = newsContent.title;
   content.appendChild(title);
 });
 for (var i = 0; i < 10; i++) {
+  var div = document.createElement('div');
   var li = document.createElement('li');
   var a = document.createElement('a');
-  ul.appendChild(li);
-  li.appendChild(a);
-  a.href = "#".concat(newsFeed[i].id);
-  a.innerHTML = "".concat(newsFeed[i].title, " (").concat(newsFeed[i].comments_count, ")");
+  div.innerHTML = /* html */"\n        <li>\n            <a href=\"#".concat(newsFeed[i].id, "\">\n                ").concat(newsFeed[i].title, " (").concat(newsFeed[i].comments_count, ")\n            </a>\n        </li>\n    ");
+  ul.appendChild(div.firstElementChild);
 }
 container.appendChild(ul);
 container.appendChild(content);
