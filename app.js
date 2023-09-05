52453c544d5481d4f37f8ee79476eda4307cfbc5
Taegwan Youn <tkyoun0421@naver.com>
2023-09-05 15:59:47 +0900

    두 개의 화면을 가진 웹앱

--- app.js
+++ app.js
@@ -1,15 +1,38 @@
+const container = document.getElementById('root');
 const ajax = new XMLHttpRequest();
+const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
+const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 ajax.open('GET', NEWS_URL, false);
 ajax.send();
 
 const newsFeed = JSON.parse(ajax.response);
 const ul = document.createElement('ul');
-document.getElementById('root').appendChild(ul);
+
+window.addEventListener('hashchange', () => {
+    const id = location.hash.slice(1);
+
+    ajax.open('GET', CONTENT_URL.replace('@id', id), false);
+    ajax.send();
+
+    const newsContent = JSON.parse(ajax.response);
+    const title = document.createElement('h1');
+
+    title.innerHTML = newsContent.title;
+    content.appendChild(title);
+});
 
 for (let i = 0; i < 10; i++) {
     const li = document.createElement('li');
-    ul.appendChild(li);
+    const a = document.createElement('a');
 
-    li.innerHTML = newsFeed[i].title;
+    ul.appendChild(li);
+    li.appendChild(a);
+
+    a.href = `#${newsFeed[i].id}`;
+
+    a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`;
 }
+
+container.appendChild(ul);
+container.appendChild(content);
--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -118,18 +118,34 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"app.js":[function(require,module,exports) {
+var container = document.getElementById('root');
 var ajax = new XMLHttpRequest();
+var content = document.createElement('div');
 var NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
+var CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 ajax.open('GET', NEWS_URL, false);
 ajax.send();
 var newsFeed = JSON.parse(ajax.response);
 var ul = document.createElement('ul');
-document.getElementById('root').appendChild(ul);
+window.addEventListener('hashchange', function () {
+  var id = location.hash.slice(1);
+  ajax.open('GET', CONTENT_URL.replace('@id', id), false);
+  ajax.send();
+  var newsContent = JSON.parse(ajax.response);
+  var title = document.createElement('h1');
+  title.innerHTML = newsContent.title;
+  content.appendChild(title);
+});
 for (var i = 0; i < 10; i++) {
   var li = document.createElement('li');
+  var a = document.createElement('a');
   ul.appendChild(li);
-  li.innerHTML = newsFeed[i].title;
+  li.appendChild(a);
+  a.href = "#".concat(newsFeed[i].id);
+  a.innerHTML = "".concat(newsFeed[i].title, " (").concat(newsFeed[i].comments_count, ")");
 }
+container.appendChild(ul);
+container.appendChild(content);
 },{}],"../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
