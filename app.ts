bdf47e926e13bd0d35ec98bca0a88bb1548b56c6
Taegwan Youn <tkyoun0421@naver.com>
2023-09-07 14:34:42 +0900

    상속과 믹스인

--- app.ts
+++ app.ts
@@ -37,6 +37,44 @@ const store: Store = {
     feeds: [],
 };
 
+function applyApiMixins(targetClass: any, baseClasses: any) {
+    baseClasses.forEach((baseClass: any) => {
+        Object.getOwnPropertyNames(baseClass.prototype).forEach((name) => {
+            const descriptor = Object.getOwnPropertyDescriptor(baseClass.prototype, name);
+
+            if (descriptor) {
+                Object.defineProperty(targetClass.prototype, name, descriptor);
+            }
+        });
+    });
+}
+
+class Api {
+    getRequest<AjaxResponse>(url: string): AjaxResponse {
+        ajax.open('GET', url, false);
+        ajax.send();
+
+        return JSON.parse(ajax.response);
+    }
+}
+
+class NewsFeedApi {
+    getData(): NewsFeed[] {
+        return this.getRequest<NewsFeed[]>(NEWS_URL);
+    }
+}
+
+class NewsDetailApi {
+    getData(id: string): NewsDetail {
+        return this.getRequest<NewsDetail>(CONTENT_URL.replace('@id', id));
+    }
+}
+
+interface NewsFeedApi extends Api {}
+interface NewsDetailApi extends Api {}
+applyApiMixins(NewsFeedApi, [Api]);
+applyApiMixins(NewsDetailApi, [Api]);
+
 function getData<AjaxResponse>(url: string): AjaxResponse {
     ajax.open('GET', url, false);
     ajax.send();
@@ -61,6 +99,7 @@ function updateView(html: string): void {
 }
 
 function newsFeed(): void {
+    const api = new NewsFeedApi();
     let newsFeed: NewsFeed[] = store.feeds;
     const newsList = [];
     let template = `
@@ -89,7 +128,7 @@ function newsFeed(): void {
     `;
 
     if (newsFeed.length === 0) {
-        newsFeed = store.feeds = makeFeeds(getData<NewsFeed[]>(NEWS_URL));
+        newsFeed = store.feeds = makeFeeds(api.getData());
     }
 
     for (let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
@@ -125,7 +164,8 @@ function newsFeed(): void {
 
 function newsDetail(): void {
     const id = location.hash.slice(7);
-    const newsContent = getData<NewsDetail>(CONTENT_URL.replace('@id', id));
+    const api = new NewsDetailApi();
+    const newsDetail: NewsDetail = api.getData(id);
     let template = `
     <div class="bg-gray-600 min-h-screen pb-8">
       <div class="bg-white text-xl">
@@ -144,9 +184,9 @@ function newsDetail(): void {
       </div>
 
         <div class="h-full border rounded-xl bg-white m-6 p-4 ">
-            <h2>${newsContent.title}</h2>
+            <h2>${newsDetail.title}</h2>
             <div class="text-gray-400 h-20">
-                ${newsContent.content}
+                ${newsDetail.content}
             </div>
 
             {{__comments__}}
@@ -161,7 +201,7 @@ function newsDetail(): void {
         }
     }
 
-    updateView(template.replace('{{__comments__}}', makeComment(newsContent.comments)));
+    updateView(template.replace('{{__comments__}}', makeComment(newsDetail.comments)));
 }
 
 function makeComment(comments: NewsComment[]): string {
--- dist/app.c61986b1.js
+++ dist/app.c61986b1.js
@@ -129,6 +129,41 @@ var store = {
   currentPage: 1,
   feeds: []
 };
+function applyApiMixins(targetClass, baseClasses) {
+  baseClasses.forEach(function (baseClass) {
+    Object.getOwnPropertyNames(baseClass.prototype).forEach(function (name) {
+      var descriptor = Object.getOwnPropertyDescriptor(baseClass.prototype, name);
+      if (descriptor) {
+        Object.defineProperty(targetClass.prototype, name, descriptor);
+      }
+    });
+  });
+}
+var Api = /** @class */function () {
+  function Api() {}
+  Api.prototype.getRequest = function (url) {
+    ajax.open('GET', url, false);
+    ajax.send();
+    return JSON.parse(ajax.response);
+  };
+  return Api;
+}();
+var NewsFeedApi = /** @class */function () {
+  function NewsFeedApi() {}
+  NewsFeedApi.prototype.getData = function () {
+    return this.getRequest(NEWS_URL);
+  };
+  return NewsFeedApi;
+}();
+var NewsDetailApi = /** @class */function () {
+  function NewsDetailApi() {}
+  NewsDetailApi.prototype.getData = function (id) {
+    return this.getRequest(CONTENT_URL.replace('@id', id));
+  };
+  return NewsDetailApi;
+}();
+applyApiMixins(NewsFeedApi, [Api]);
+applyApiMixins(NewsDetailApi, [Api]);
 function getData(url) {
   ajax.open('GET', url, false);
   ajax.send();
@@ -148,11 +183,12 @@ function updateView(html) {
   }
 }
 function newsFeed() {
+  var api = new NewsFeedApi();
   var newsFeed = store.feeds;
   var newsList = [];
   var template = "\n        <div class=\"bg-gray-600 min-h-screen\">\n            <div class=\"bg-white text-xl\">\n                <div class=\"mx-auto px-4\">\n                    <div class=\"flex justify-between items-center py-6\">\n                        <div class=\"flex justify-start\">\n                            <h1 class=\"font-extrabold\">Hacker News</h1>\n                        </div>\n                        <div class=\"items-center justify-end\">\n                            <a href=\"#/page/{{__prev_page__}}\" class=\"text-gray-500\">\n                                Previous\n                            </a>\n                            <a href=\"#/page/{{__next_page__}}\" class=\"text-gray-500\">\n                                Next\n                            </a>\n                        </div>\n                    </div>\n                </div>\n            </div>\n            <div class=\"p-4 text-2xl text-gray-700\">\n                {{__news_feed__}}\n            </div>\n        </div>\n    ";
   if (newsFeed.length === 0) {
-    newsFeed = store.feeds = makeFeeds(getData(NEWS_URL));
+    newsFeed = store.feeds = makeFeeds(api.getData());
   }
   for (var i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
     newsList.push("\n            <div class=\"p-6 ".concat(newsFeed[i].read ? 'bg-red-500' : 'bg-white', " mt-6 rounded-lg shadow-md transition-colors duration-500 hover:bg-green-100\">\n            <div class=\"flex\">\n                <div class=\"flex-auto\">\n                    <a href=\"#/show/").concat(newsFeed[i].id, "\">").concat(newsFeed[i].title, "</a>  \n                </div>\n                <div class=\"text-center text-sm\">\n                <div class=\"w-10 text-white bg-green-300 rounded-lg px-0 py-2\">").concat(newsFeed[i].comments_count, "</div>\n                </div>\n            </div>\n            <div class=\"flex mt-3\">\n                <div class=\"grid grid-cols-3 text-sm text-gray-500\">\n                    <div><i class=\"fas fa-user mr-1\"></i>").concat(newsFeed[i].user, "</div>\n                    <div><i class=\"fas fa-heart mr-1\"></i>").concat(newsFeed[i].points, "</div>\n                    <div><i class=\"far fa-clock mr-1\"></i>").concat(newsFeed[i].time_ago, "</div>\n                </div>  \n            </div>\n            </div>  \n            "));
@@ -164,15 +200,16 @@ function newsFeed() {
 }
 function newsDetail() {
   var id = location.hash.slice(7);
-  var newsContent = getData(CONTENT_URL.replace('@id', id));
-  var template = "\n    <div class=\"bg-gray-600 min-h-screen pb-8\">\n      <div class=\"bg-white text-xl\">\n        <div class=\"mx-auto px-4\">\n          <div class=\"flex justify-between items-center py-6\">\n            <div class=\"flex justify-start\">\n              <h1 class=\"font-extrabold\">Hacker News</h1>\n            </div>\n            <div class=\"items-center justify-end\">\n              <a href=\"#/page/".concat(store.currentPage, "\" class=\"text-gray-500\">\n                <i class=\"fa fa-times\"></i>\n              </a>\n            </div>\n          </div>\n        </div>\n      </div>\n\n        <div class=\"h-full border rounded-xl bg-white m-6 p-4 \">\n            <h2>").concat(newsContent.title, "</h2>\n            <div class=\"text-gray-400 h-20\">\n                ").concat(newsContent.content, "\n            </div>\n\n            {{__comments__}}\n\n        </div>\n    </div>\n    ");
+  var api = new NewsDetailApi();
+  var newsDetail = api.getData(id);
+  var template = "\n    <div class=\"bg-gray-600 min-h-screen pb-8\">\n      <div class=\"bg-white text-xl\">\n        <div class=\"mx-auto px-4\">\n          <div class=\"flex justify-between items-center py-6\">\n            <div class=\"flex justify-start\">\n              <h1 class=\"font-extrabold\">Hacker News</h1>\n            </div>\n            <div class=\"items-center justify-end\">\n              <a href=\"#/page/".concat(store.currentPage, "\" class=\"text-gray-500\">\n                <i class=\"fa fa-times\"></i>\n              </a>\n            </div>\n          </div>\n        </div>\n      </div>\n\n        <div class=\"h-full border rounded-xl bg-white m-6 p-4 \">\n            <h2>").concat(newsDetail.title, "</h2>\n            <div class=\"text-gray-400 h-20\">\n                ").concat(newsDetail.content, "\n            </div>\n\n            {{__comments__}}\n\n        </div>\n    </div>\n    ");
   for (var i = 0; i < store.feeds.length; i++) {
     if (store.feeds[i].id === Number(id)) {
       store.feeds[i].read = true;
       break;
     }
   }
-  updateView(template.replace('{{__comments__}}', makeComment(newsContent.comments)));
+  updateView(template.replace('{{__comments__}}', makeComment(newsDetail.comments)));
 }
 function makeComment(comments) {
   var commentString = [];
